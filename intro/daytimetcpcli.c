b09af838a74b646f8a94196e43751e2761ed8cd0
xiangxunliu <748893103@qq.com>
2024-09-21 15:49:50 +0800

    test

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -13,9 +13,14 @@ main(int argc, char **argv)
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		err_sys("socket error");
 
+	// 将结构体清0
 	bzero(&servaddr, sizeof(servaddr));
+
+	// 使用IPv4协议
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port   = htons(13);	/* daytime server */
+	
+	// 转为网络字节序
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
 
